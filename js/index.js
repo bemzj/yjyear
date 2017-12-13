606f697304128f08ae74b2888322fa8f553b62ef
bemzj <qiuzijia520@qq.com>
2017-12-13 21:43:55 +0800

    1213

--- js/index.js
+++ js/index.js
@@ -11,7 +11,9 @@ $(function(){
 	var nextTween;	
 	document.getElementById("video").onended=function(){
 		$('.video').fadeOut();
-		$('#index').css('opacity','1');
+		$('#index').show().css('opacity','1');
+		$('#bg')[0].play();
+		swiper.slideTo(0, 100, true);//切换到第一个slide，速度为1秒
 		$('.nextBox').animate({'bottom':'0.3rem','opacity':'0'},600,function(){
 			$(this).css({'bottom':'0.8rem','opacity':'1'});
 		});
@@ -20,21 +22,23 @@ $(function(){
 				$(this).css({'bottom':'0.8rem','opacity':'1'});
 			});
 		},600);
-		$('.logo').addClass('animated bounceInRight');
-		$('.title').addClass('animated fadeInDown');
-		$('.timeTitle').addClass('animated rotateIn');
-		$('.timeBox div').eq(2).addClass('animated bounceInLeft');
-		$('.timeBox div').eq(3).addClass('animated bounceInLeft');
-		$('.timeBox div').eq(0).addClass('animated bounceInRight');
-		$('.timeBox div').eq(1).addClass('animated bounceInRight');
-		$('.address p').eq(0).addClass('animated fadeInDown');
-		$('.address p').eq(1).addClass('animated fadeInUp');
+		$('.logo').show().addClass('animated bounceInRight');
+		$('.title').show().addClass('animated fadeInDown');
+		$('.timeTitle').show().addClass('animated rotateIn');
+		$('.timeBox div').show().eq(2).addClass('animated bounceInLeft');
+		$('.timeBox div').show().eq(3).addClass('animated bounceInLeft');
+		$('.timeBox div').show().eq(0).addClass('animated bounceInRight');
+		$('.timeBox div').show().eq(1).addClass('animated bounceInRight');
+		$('.address p').show().eq(0).addClass('animated fadeInDown');
+		$('.address p').show().eq(1).addClass('animated fadeInUp');
 	};
 	//跳过
 	$('#over').bind('click',function(){
 		$('#video')[0].pause();
 		$('.video').fadeOut();
-		$('#index').css('opacity','1');
+		$('#bg')[0].play();
+		$('#index').show().css('opacity','1');
+		swiper.slideTo(0, 100, true);//切换到第一个slide，速度为1秒
 		$('.nextBox').animate({'bottom':'0.3rem','opacity':'0'},600,function(){
 			$(this).css({'bottom':'0.8rem','opacity':'1'});
 		});
@@ -43,15 +47,15 @@ $(function(){
 				$(this).css({'bottom':'0.8rem','opacity':'1'});
 			});
 		},600);
-		$('.logo').addClass('animated bounceInRight');
-		$('.title').addClass('animated fadeInDown');
-		$('.timeTitle').addClass('animated rotateIn');
-		$('.timeBox div').eq(2).addClass('animated bounceInLeft');
-		$('.timeBox div').eq(3).addClass('animated bounceInLeft');
-		$('.timeBox div').eq(0).addClass('animated bounceInRight');
-		$('.timeBox div').eq(1).addClass('animated bounceInRight');
-		$('.address p').eq(0).addClass('animated fadeInDown');
-		$('.address p').eq(1).addClass('animated fadeInUp');
+		$('.logo').show().addClass('animated bounceInRight');
+		$('.title').show().addClass('animated fadeInDown');
+		$('.timeTitle').show().addClass('animated rotateIn');
+		$('.timeBox div').show().eq(2).addClass('animated bounceInLeft');
+		$('.timeBox div').show().eq(3).addClass('animated bounceInLeft');
+		$('.timeBox div').show().eq(0).addClass('animated bounceInRight');
+		$('.timeBox div').show().eq(1).addClass('animated bounceInRight');
+		$('.address p').show().eq(0).addClass('animated fadeInDown');
+		$('.address p').show().eq(1).addClass('animated fadeInUp');
 	});
 	//拒接
 	$('.refuse a').on('click',function(){
@@ -111,6 +115,7 @@ $(function(){
 	});
 	//提交
 	$('.btn button').bind('click',function(){
+		
 		clearInterval(nextTween);
 		$('.nextBox').css({'bottom':'0.8rem','opacity':'1'});
 		$('#index').fadeOut(500);
@@ -125,10 +130,111 @@ $(function(){
 		$('.message li').eq(4).addClass('animated slideInLeft');
 		$('.submit').addClass('animated fadeInUp');
 	});
+	var ct1;
+	var ct2;
+	var lightTween;
+	var rTween;
+	var saoTween;
+	var cTween1;
+	var cTween2;
 	$('.submit').bind('click',function(){
 		$('#apply').fadeOut(500);
-		$('#hand').fadeIn(500);	
+		$('#hand').fadeIn(500);
+		//指纹
+		
+		var lrule = true;
+		lightTween = setInterval(function(){
+			if(lrule==true)
+			{
+				lrule = false;
+				$('.light').animate({'opacity':0.5},500);
+			}else{
+				lrule = true;
+				$('.light').animate({'opacity':1},500);
+			}	
+		},500);
+		
+		rTime=2;
+		rTween = setInterval(function(){
+			$('.rBox img').attr('src','img/r'+rTime+'.png');
+			rTime++;
+			if(rTime==4)
+			{
+				rTime=1;
+			}
+		},750);
+			
+		saoTween = setInterval(function(){
+			$('.sao img').animate({'bottom':"-5%"},2500,function(){
+				$(this).css('bottom',"100%");
+			});
+		},2500);
+		
+		var cTime1 = 0;
+		ct1 = 0.1;
+		cTween1 = setInterval(function(){
+			cTime1+=ct1;
+			$('.circleOut>img').css('transform','rotate('+cTime1+'deg)');
+		},20);
+		
+		
+		var cTime2 = 0;
+		var ct2 = 0.2;
+		cTween2 = setInterval(function(){
+			cTime2-=ct2;
+			$('.circleInBox>img').css('transform','rotate('+cTime2+'deg)');
+		},20);
+		
 	});
-	//指纹
 	
-});
+	
+	
+	$('#press').bind('click',function(){
+		ct1=10;
+		ct2=10;
+		$('.input').hide();
+		$('.now').show();
+		var iTween;
+		iRule = true;
+		
+		iTween = setInterval(function(){
+			if(iRule == true)
+			{
+				iRule = false;
+				$('.now').css('opacity','0');
+			}else{
+				iRule = true;
+				$('.now').css('opacity','1');
+			}
+		},250);
+		setTimeout(function(){
+			$('#pressed')[0].play();	
+			setTimeout(function(){
+				clearInterval(lightTween);
+				clearInterval(rTween);
+				clearInterval(saoTween);
+				clearInterval(iTween);
+				clearInterval(cTween1);
+				clearInterval(cTween2);
+				$('#hand').fadeOut(500);
+				$('#success').fadeIn(500);
+				$('.slogo').show().addClass('animated bounceInRight');
+				$('.stitle').show().addClass('animated tada');
+				$('.wxHead').show().addClass('animated rubberBand');
+				$('.namestore').show().addClass('animated rotateIn');
+				$('.sBox').show().addClass('animated bounceInDown');
+				$('.return').show().addClass('animated bounceInUp');
+				$('.return button').bind('click',function(){					
+					$('#success').hide();
+					$('#phoneCome').fadeIn(500,function(){
+						$('#pMusic')[0].play();
+					});
+					$('#bg')[0].pause();
+				});
+			},1000);
+		},2000);
+		
+	});
+	
+	
+})
